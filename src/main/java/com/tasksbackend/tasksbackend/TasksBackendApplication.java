53a94e9f7ccfa4219ec705712906d925186bd57f
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-02-24 19:38:08 -0300

    commit 240220231938

---
+++ src/main/java/com/tasksbackend/tasksbackend/TasksBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tasksbackend.tasksbackend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello word");
+	}
+
+}
